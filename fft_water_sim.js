4ab6786d6618fbe77f83bbbcbe3a90fc51239ed2
Majkejl <misicek03@gmail.com>
2026-05-11 03:14:19 +0200

    update emscripten build

--- fft_water_sim.js
+++ fft_water_sim.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\misic\AppData\Local\Temp\tmphwe09f88.js
+// include: C:\Users\misic\AppData\Local\Temp\tmpn818d42p.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -230,21 +230,21 @@ Module['FS_createPath']("/C:/Users/misic/source/repos/fft_water_sim_/resources",
 
   })();
 
-// end include: C:\Users\misic\AppData\Local\Temp\tmphwe09f88.js
-// include: C:\Users\misic\AppData\Local\Temp\tmpirhfta5j.js
+// end include: C:\Users\misic\AppData\Local\Temp\tmpn818d42p.js
+// include: C:\Users\misic\AppData\Local\Temp\tmpyrrt4bui.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\misic\AppData\Local\Temp\tmpirhfta5j.js
-// include: C:\Users\misic\AppData\Local\Temp\tmpp1mok5jf.js
+  // end include: C:\Users\misic\AppData\Local\Temp\tmpyrrt4bui.js
+// include: C:\Users\misic\AppData\Local\Temp\tmp97j40d4l.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\misic\AppData\Local\Temp\tmpp1mok5jf.js
+  // end include: C:\Users\misic\AppData\Local\Temp\tmp97j40d4l.js
 
 
 // Sometimes an existing Module object exists with properties
